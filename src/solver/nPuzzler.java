df4ace26e05416964b8b9e86fab1b6c3e0fe8411
Dylan <21029693+Dy1ski@users.noreply.github.com>
2018-04-19 19:29:21 +1000

    Add files via upload

--- src/solver/nPuzzler.java
+++ src/solver/nPuzzler.java
@@ -8,32 +8,32 @@ import java.util.*;
  */
 class nPuzzler
 {
-	
+
 	//the number of methods programmed into nPuzzler
-	public static final int METHOD_COUNT = 2;
+	public static final int METHOD_COUNT = 4;
 	public static nPuzzle gPuzzle;
 	public static SearchMethod[] lMethods;
-	
+
 	public static void main(String[] args)
 	{
 		//Create method objects
 		InitMethods();
-		
+
 		//args contains:
 		//  [0] - filename containing puzzle(s)
 		//  [1] - method name
-		
+
 		if(args.length < 2) {
 			System.out.println("Usage: nPuzzler <filename> <search-method>.");
-			System.exit(1);			
+			System.exit(1);
 		}
-		
+
 		//Get the puzzle from the file
 		gPuzzle = readProblemFile(args[0]);
-		
+
 		String method = args[1];
 		SearchMethod thisMethod = null;
-		
+
 		//determine which method the user wants to use to solve the puzzles
 		for(int i = 0; i < METHOD_COUNT; i++)
 		{
@@ -44,7 +44,7 @@ class nPuzzler
 				thisMethod = lMethods[i];
 			}
 		}
-		
+
 		//Has the method been implemented?
 		if(thisMethod == null)
 		{
@@ -52,10 +52,10 @@ class nPuzzler
 			System.out.println("Search method identified by " + method + " not implemented. Methods are case sensitive.");
 			System.exit(1);
 		}
-		
+
 		//Solve the puzzle, using the method that the user chose
 		direction[] thisSolution = thisMethod.Solve(gPuzzle);
-		
+
 		//Print information about this solution
 		System.out.println(args[0] + "   " + method + "   " + thisMethod.Searched.size());
 		if(thisSolution == null)
@@ -76,48 +76,48 @@ class nPuzzler
 		thisMethod.reset();
 		System.exit(0);
 	}
-	
+
 	private static void InitMethods()
 	{
 		lMethods = new SearchMethod[METHOD_COUNT];
 		lMethods[0] = new BFSStrategy();
 		lMethods[1] = new GreedyBestFirstStrategy();
 		lMethods[2] = new DepthFirstSearch();
-	//  	lMethods[3] = new Astar(); Implement this in the next few days
+	  lMethods[3] = new Astar();
 	}
-	
-	private static nPuzzle readProblemFile(String fileName) // this allow only one puzzle to be specified in a problem file 
+
+	private static nPuzzle readProblemFile(String fileName) // this allow only one puzzle to be specified in a problem file
 	{
-		
+
 		try
 		{
 			//create file reading objects
 			FileReader reader = new FileReader(fileName);
 			BufferedReader puzzle = new BufferedReader(reader);
 			nPuzzle result;
-			
+
 			String puzzleDimension = puzzle.readLine();
 			//split the string by letter "x"
 			String[] bothDimensions = puzzleDimension.split("x");
-		
+
 			//work out the "physical" size of the puzzle
 			//here we only deal with NxN puzzles, so the puzzle size is taken to be the first number
 			int puzzleSize = Integer.parseInt(bothDimensions[0]);
-			
+
 			int[][] startPuzzleGrid = new int[puzzleSize][puzzleSize];
 			int[][] goalPuzzleGrid = new int[puzzleSize][puzzleSize];
-			
+
 			//fill in the start state
 			String startStateString = puzzle.readLine();
 			startPuzzleGrid = ParseStateString(startStateString, startPuzzleGrid, puzzleSize);
-			
+
 			//fill in the end state
 			String goalStateString = puzzle.readLine();
 			goalPuzzleGrid = ParseStateString(goalStateString, goalPuzzleGrid, puzzleSize);
-			
+
 			//create the nPuzzle object...
 			result = new nPuzzle(startPuzzleGrid, goalPuzzleGrid);
-						
+
 			puzzle.close();
 			return result;
 		}
@@ -135,39 +135,39 @@ class nPuzzler
 			System.out.println("If you're accessing this file over a network, try making a local copy.");
 			System.exit(1);
 		}
-		
+
 		//this code should be unreachable. This statement is simply to satisfy Eclipse.
 		return null;
 	}
-	
+
 	private static int[][] ParseStateString(String stateString, int[][] puzzleGrid, int pWidth)
 	{
 		//Parse state string converts the text file's format for each puzzle into
 		// multidimensional arrays.
-		
+
 		//split the string by spaces
 		String[] tileLocations = stateString.split(" ");
-		
+
 		// the top-left corner of the puzzle has a coordinate of [0,0]
-		int x = 0;	
+		int x = 0;
 		int y = 0;
-		
+
 		for(int i = 0; i < tileLocations.length; i++)
 		{
 			//tileLocations[i] holds the (i + 1)th tile
 			int tileNumber = Integer.parseInt(tileLocations[i]);
-			
+
 			//now, check the location of this tile
 			if (x >= pWidth) {
 				//reset x to 0 and go to next row (increase y by 1)
 				x = 0;
 				y++;
 			}
-			
+
 			puzzleGrid[x][y] = tileNumber;
 			x++;
 		}
-		
+
 		return puzzleGrid;
 	}
 }
